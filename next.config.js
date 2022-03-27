7eeab1256fda6bfc22d5e5e287a3afcd3d5ae21a
PradyumnaCharate <91411558+PradyumnaCharate@users.noreply.github.com>
2022-03-27 23:14:26 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-            destination: 'https://pradyumna-poirot.netlify.app/:path*',
-      },
-    ]
-  },
-}
+
 
 
