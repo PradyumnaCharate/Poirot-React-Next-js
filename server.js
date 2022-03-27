602acdd48ffedcefb406db86c1e11ee720116b16
PradyumnaCharate <91411558+PradyumnaCharate@users.noreply.github.com>
2022-03-27 23:09:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -137,19 +137,7 @@ nextApp.prepare().then(() => {
 
   //because Nextjs pages are created at server (Server side rendering) we must do this
   app.all("*", (req, res) => handle(req, res));
-   app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-  if (req.method == "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-
-  next();
-});
+   
 
   server.listen(PORT, err => {
     if (err) throw err;
