6057aa96d723b9c654e2fecb45d67544020719e0
PradyumnaCharate <91411558+PradyumnaCharate@users.noreply.github.com>
2022-03-28 11:21:33 +0530

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -8,11 +8,10 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const isEmail = require("validator/lib/isEmail");
 const authMiddleware = require("../middleware/authMiddleware");
-const cors = require('./cors');
-.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+
 
 //To verify header in request and send follow stats back
-router.get("/",cors.corsWithOptions authMiddleware, async (req, res) => {
+router.get("/", authMiddleware, async (req, res) => {
   const { userId } = req; 
 
   try {
@@ -29,7 +28,7 @@ router.get("/",cors.corsWithOptions authMiddleware, async (req, res) => {
 
 
 //To validate login request
-router.post("/",cors.corsWithOptions async (req, res) => {
+router.post("/", async (req, res) => {
   const { email, password } = req.body.user;
 
   if (!isEmail(email)) return res.status(401).send("Invalid Email");
